278a696bc29b3de4313bd879d731e4bcdc271803
saurabh <git@saurabhn.com>
2025-12-28 11:10:38 +0530

    Further simplify worker - use URL string instead of Request object

--- worker.js
+++ worker.js
@@ -8,15 +8,10 @@ export default {
 
     // If we get a 404 and it's not a file with an extension, serve index.html
     // This allows React Router to handle the routing client-side
-    if (response.status === 404) {
-      // Check if this looks like a page route (no file extension)
-      const hasExtension = /\.[a-zA-Z0-9]+$/.test(pathname);
-
-      if (!hasExtension) {
-        // Serve index.html for SPA routes
-        const indexRequest = new Request(new URL('/index.html', url), request);
-        response = await env.ASSETS.fetch(indexRequest);
-      }
+    if (response.status === 404 && !pathname.match(/\.[a-zA-Z0-9]+$/)) {
+      // Serve index.html for SPA routes
+      url.pathname = '/index.html';
+      response = await env.ASSETS.fetch(url.toString());
     }
 
     return response;
